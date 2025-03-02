821d1494b7bee22694dee72cb11fe742dac4be55
Mur-Muring <Ezhova.13@yandex.ru>
2025-03-02 22:12:22 +0700

    Часть 2 ревью 3 косяк в CommentRepository

--- main-service/src/main/java/ru.practicum.ewm/comments/CommentRepository.java
+++ main-service/src/main/java/ru.practicum.ewm/comments/CommentRepository.java
@@ -1,28 +1,29 @@
 package ru.practicum.ewm.comments;
 
-import org.hibernate.annotations.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.comment.CountCommentsByEventDto;
+import ru.practicum.ewm.comments.Comment;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
     List<Comment> findAllByEvent_Id(Long eventId, Pageable pageable);
 
     List<Comment> findByAuthor_Id(Long userId);
 
     Optional<Comment> findByAuthor_IdAndId(Long userId, Long id);
 
-    @Query("select new CountCommentsByEventDto(c.event.id, COUNT(c)) " +
-            "from comments as c where c.event.id in ?1 " +
+    @Query("SELECT new ru.practicum.ewm.comment.CountCommentsByEventDto(c.event.id, COUNT(c)) " +
+            "FROM Comment c WHERE c.event.id IN ?1 " +
             "GROUP BY c.event.id")
     List<CountCommentsByEventDto> countCommentByEvent(List<Long> eventIds);
 
-    @Query("select c " +
-            "from comments as c " +
-            "where lower(c.text) like lower(concat('%', ?1, '%') )")
+    @Query("SELECT c FROM Comment c WHERE LOWER(c.text) LIKE LOWER(CONCAT('%', ?1, '%'))")
     List<Comment> search(String text, Pageable pageable);
 }
