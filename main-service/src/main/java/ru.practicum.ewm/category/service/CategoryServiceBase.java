75d46cca716d269c71a452f53da1c55e4b2fe2ae
Mur-Muring <Ezhova.13@yandex.ru>
2025-03-04 19:44:41 +0700

    Часть 2 ревью 5

--- main-service/src/main/java/ru.practicum.ewm/category/service/CategoryServiceBase.java
+++ main-service/src/main/java/ru.practicum.ewm/category/service/CategoryServiceBase.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.category.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
